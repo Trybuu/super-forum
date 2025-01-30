604c973ab56500d1721ab96501201f7cf22785ea
Marek Trybuła <trybu001@gmail.com>
2025-01-30 19:01:40 +0100

    Left menu with loading fake data

--- src/components/LeftMenu.tsx
+++ src/components/LeftMenu.tsx
@@ -1,13 +1,26 @@
+import { useEffect, useState } from 'react'
 import { useWindowDimensions } from '../hooks/useWindowDimensions'
+import Category from '../models/Category'
+import './LeftMenu.css'
+import { getCategories } from '../services/DataService'
 
 const LeftMenu = () => {
+  const [categories, setCategories] = useState<JSX.Element>(<p>Menu z lewej</p>)
   const { width } = useWindowDimensions()
+
+  useEffect(() => {
+    getCategories().then((categories: Array<Category>) => {
+      const cats = categories.map((cat) => <li key={cat.id}>{cat.name}</li>)
+
+      setCategories(<ul className="category">{cats}</ul>)
+    })
+  }, [])
 
   if (width <= 768) {
     return null
   }
 
-  return <div className="leftmenu">Menu z lewej</div>
+  return <div className="leftmenu">{categories}</div>
 }
 
 export default LeftMenu
