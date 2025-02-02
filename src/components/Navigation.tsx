6aff4318860a3f583b34239b26f6716f6656b0ac
Marek Trybuła <trybu001@gmail.com>
2025-02-02 17:11:39 +0100

    Navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,12 +1,28 @@
 import { FaBars } from 'react-icons/fa'
 import { useWindowDimensions } from '../hooks/useWindowDimensions'
+import React, { useState } from 'react'
+import ReactModal from 'react-modal'
+import SideBarMenus from './sidebar/SideBarMenu'
 
 const Navigation = () => {
   const { width } = useWindowDimensions()
+  const [showMenu, setShowMenu] = useState<boolean>(false)
 
   const getMobileMenu = () => {
     if (width <= 768) {
-      return <FaBars />
+      return (
+        <React.Fragment>
+          <FaBars onClick={() => setShowMenu(true)} />
+
+          <ReactModal
+            isOpen={showMenu}
+            onRequestClose={() => setShowMenu(false)}
+            shouldCloseOnOverlayClick={true}
+          >
+            <SideBarMenus />
+          </ReactModal>
+        </React.Fragment>
+      )
     } else {
       return null
     }
